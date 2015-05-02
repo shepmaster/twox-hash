eebd5b5ab5b4abaebb69fc06f8f41650ca5c82cc
Jake Goulding <jake.goulding@gmail.com>
2015-05-02 15:09:10 -0400

    Add example that calculates the hash of provided files

---
+++ src/bin/hash_file.rs
@@ -0,0 +1,28 @@
+extern crate hash;
+
+use std::env;
+use std::hash::Hasher;
+use std::fs::File;
+use std::io::{BufRead,BufReader};
+use hash::XxHash;
+
+fn main() {
+    for arg in env::args().skip(1) {
+        let f = File::open(&arg).unwrap();
+        let mut f = BufReader::new(f);
+
+        let mut hasher = XxHash::with_seed(0);
+
+        loop {
+            let consumed = {
+                let bytes = f.fill_buf().unwrap();
+                if bytes.len() == 0 { break }
+                hasher.write(bytes);
+                bytes.len()
+            };
+            f.consume(consumed);
+        }
+
+        println!("{:16x}   {}", hasher.finish(), arg);
+    }
+}
