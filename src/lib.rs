5d4f3cf35e6045ff4e0bf98b186e9fa54a3afd56
Jake Goulding <jake.goulding@gmail.com>
2015-05-08 22:50:14 -0400

    Add library documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+//! A Rust implementation of the [XXHash] algorithm.
+//!
+//! [XXHash]: https://github.com/Cyan4973/xxHash
+//!
+//! ### With a fixed seed
+//!
+//! ```rust
+//! #![feature(std_misc)]
+//!
+//! use std::collections::HashMap;
+//! use std::collections::hash_state::DefaultState;
+//! use twox_hash::XxHash;
+//!
+//! let mut hash: HashMap<_, _, DefaultState<XxHash>> = Default::default();
+//! hash.insert(42, "the answer");
+//! assert_eq!(hash.get(&42), Some(&"the answer"));
+//! ```
+//!
+//! ### With a random seed
+//!
+//! ```rust
+//! use std::collections::HashMap;
+//! use twox_hash::RandomXxHashState;
+//!
+//! let mut hash: HashMap<_, _, RandomXxHashState> = Default::default();
+//! hash.insert(42, "the answer");
+//! assert_eq!(hash.get(&42), Some(&"the answer"));
+//! ```
+
 #![feature(std_misc)]
 #![cfg_attr(test, feature(test))]
 
