41207e9f64ada2a8e8c087bf4a4b61dc7bfd0ea2
Travis-CI <builder@travis>
2016-11-19 20:02:24 +0000

    Deployed to Github Pages

---
+++ implementors/core/hash/trait.BuildHasher.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libc"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.BuildHasher.html' title='core::hash::BuildHasher'>BuildHasher</a> for <a class='struct' href='https://doc.rust-lang.org/nightly/std/collections/hash/map/struct.RandomState.html' title='std::collections::hash::map::RandomState'>RandomState</a>",];implementors["twox_hash"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.BuildHasher.html' title='core::hash::BuildHasher'>BuildHasher</a> for <a class='struct' href='twox_hash/struct.RandomXxHashBuilder.html' title='twox_hash::RandomXxHashBuilder'>RandomXxHashBuilder</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/hash/trait.Hasher.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libc"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html' title='core::hash::Hasher'>Hasher</a> for <a class='struct' href='https://doc.rust-lang.org/nightly/std/collections/hash/map/struct.DefaultHasher.html' title='std::collections::hash::map::DefaultHasher'>DefaultHasher</a>",];implementors["twox_hash"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html' title='core::hash::Hasher'>Hasher</a> for <a class='struct' href='twox_hash/struct.XxHash.html' title='twox_hash::XxHash'>XxHash</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
