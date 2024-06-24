f9ff61c0bdd12f8ef452a09758b76550ee0bf66f
Jake Goulding <jake.goulding@gmail.com>
2024-06-24 11:01:30 -0400

    Use threads for the CLI

--- renu-sum/src/main.rs
+++ renu-sum/src/main.rs
@@ -3,37 +3,78 @@ use std::{
     fs::File,
     io::Read,
     path::{Path, PathBuf},
+    sync::mpsc,
+    thread,
 };
 use xx_renu::XxHash64;
 
-type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+const BUFFER_SIZE: usize = 16 * 1024;
+const BUFFER_COUNT: usize = 8;
 
 fn main() -> Result<()> {
-    let mut buffer = vec![0; 32 * 1024 * 1024];
-
     for path in env::args_os().skip(1) {
         let path = PathBuf::from(path);
-        let hash = hash_one_file(&path, &mut buffer)?;
+        let hash = hash_one_file(&path)?;
         eprintln!("{hash:x}  {}", path.display());
     }
 
     Ok(())
 }
 
-fn hash_one_file(path: &Path, buffer: &mut [u8]) -> Result<u64> {
+fn hash_one_file(path: &Path) -> Result<u64> {
     let mut file = File::open(path)?;
     let mut hasher = XxHash64::with_seed(0);
 
-    loop {
-        let n_bytes = file.read(buffer)?;
-        if n_bytes == 0 {
-            break;
-        }
+    let (tx, rx) = mpsc::sync_channel(BUFFER_COUNT);
+    let (tx2, rx2) = mpsc::sync_channel(BUFFER_COUNT);
 
-        let valid = &buffer[..n_bytes];
-
-        hasher.write(valid);
+    for _ in 0..BUFFER_COUNT {
+        tx.send(vec![0; BUFFER_SIZE])
+            .expect("Must be able to populate initial buffers");
     }
+
+    thread::scope(|scope| {
+        let t1 = scope.spawn(move || {
+            while let Ok(mut buffer) = rx.recv() {
+                let n_bytes = file.read(&mut buffer)?;
+
+                if n_bytes == 0 {
+                    break;
+                }
+
+                tx2.send((buffer, n_bytes))?;
+            }
+
+            Ok::<_, Error>(())
+        });
+
+        let t2 = scope.spawn({
+            let hasher = &mut hasher;
+            move || {
+                while let Ok((buffer, n_bytes)) = rx2.recv() {
+                    let valid = &buffer[..n_bytes];
+
+                    hasher.write(valid);
+
+                    if tx.send(buffer).is_err() {
+                        // The reading thread has exited and there's
+                        // nowhere to return this buffer to.
+                        continue;
+                    }
+                }
+
+                Ok::<_, Error>(())
+            }
+        });
+
+        t1.join().unwrap()?;
+        t2.join().unwrap()?;
+
+        Ok::<_, Error>(())
+    })?;
 
     let hash = hasher.finish();
     Ok(hash)
