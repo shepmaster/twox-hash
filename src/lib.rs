198fe0b8fb5ce2fbe3477cb2be86134fe542822d
Jake Goulding <jake.goulding@gmail.com>
2024-06-24 11:01:07 -0400

    Extract a helper

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ const PRIME64_5: u64 = 0x27D4EB2F165667C5;
 struct AlignedData([u8; 32]);
 
 impl AlignedData {
-    fn as_u64s(&self) -> &[u64; 4] {
+    const fn as_u64s(&self) -> &[u64; 4] {
         // SAFETY: We are guaranteed to be aligned
         unsafe { mem::transmute(&self.0) }
     }
@@ -59,9 +59,61 @@ impl Buffer {
     }
 }
 
+struct Accumulators([u64; 4]);
+
+impl Accumulators {
+    const fn new(seed: u64) -> Self {
+        Self([
+            seed.wrapping_add(PRIME64_1).wrapping_add(PRIME64_2),
+            seed.wrapping_add(PRIME64_2),
+            seed,
+            seed.wrapping_sub(PRIME64_1),
+        ])
+    }
+
+    fn write(&mut self, lanes: [u64; 4]) {
+        let [acc1, acc2, acc3, acc4] = &mut self.0;
+        let [lane1, lane2, lane3, lane4] = lanes;
+        // todo: little-endian transform
+
+        *acc1 = round(*acc1, lane1);
+        *acc2 = round(*acc2, lane2);
+        *acc3 = round(*acc3, lane3);
+        *acc4 = round(*acc4, lane4);
+    }
+
+    const fn finish(&self) -> u64 {
+        let [acc1, acc2, acc3, acc4] = self.0;
+
+        let mut acc = {
+            let acc1 = acc1.rotate_left(1);
+            let acc2 = acc2.rotate_left(7);
+            let acc3 = acc3.rotate_left(12);
+            let acc4 = acc4.rotate_left(18);
+
+            acc1.wrapping_add(acc2)
+                .wrapping_add(acc3)
+                .wrapping_add(acc4)
+        };
+
+        acc = Self::merge_accumulator(acc, acc1);
+        acc = Self::merge_accumulator(acc, acc2);
+        acc = Self::merge_accumulator(acc, acc3);
+        acc = Self::merge_accumulator(acc, acc4);
+
+        acc
+    }
+
+    const fn merge_accumulator(mut acc: u64, acc_n: u64) -> u64 {
+        acc ^= round(0, acc_n);
+        acc = acc.wrapping_mul(PRIME64_1);
+        acc.wrapping_add(PRIME64_4)
+    }
+}
+
 pub struct XxHash64 {
     seed: u64,
-    accumulators: [u64; 4],
+    accumulators: Accumulators,
     buffer: Buffer,
     length: u64,
 }
@@ -75,16 +127,10 @@ impl XxHash64 {
 
     pub fn with_seed(seed: u64) -> Self {
         // Step 1. Initialize internal accumulators
-        let accumulators = [
-            seed.wrapping_add(PRIME64_1).wrapping_add(PRIME64_2),
-            seed.wrapping_add(PRIME64_2),
-            seed,
-            seed.wrapping_sub(PRIME64_1),
-        ];
 
         Self {
             seed,
-            accumulators,
+            accumulators: Accumulators::new(seed),
             buffer: Buffer::default(),
             length: 0,
         }
@@ -94,31 +140,18 @@ impl XxHash64 {
         let len = data.len();
 
         // Step 2. Process stripes
-        // todo: dereference?
-        let [acc1, acc2, acc3, acc4] = &mut self.accumulators;
+        let (buffered_lanes, data) = self.buffer.extend(data);
 
-        let (check, data) = self.buffer.extend(data);
-
-        if let Some(&[lane1, lane2, lane3, lane4]) = check {
-            // todo: little-endian transform
-
-            *acc1 = Self::round(*acc1, lane1);
-            *acc2 = Self::round(*acc2, lane2);
-            *acc3 = Self::round(*acc3, lane3);
-            *acc4 = Self::round(*acc4, lane4);
+        if let Some(&lanes) = buffered_lanes {
+            self.accumulators.write(lanes);
         }
 
         let mut data = data;
         while let Some((chunk, rest)) = data.split_first_chunk::<32>() {
-            let [lane1, lane2, lane3, lane4] =
-                unsafe { chunk.as_ptr().cast::<[u64; 4]>().read_unaligned() };
-            // todo: little-endian transform
-
-            *acc1 = Self::round(*acc1, lane1);
-            *acc2 = Self::round(*acc2, lane2);
-            *acc3 = Self::round(*acc3, lane3);
-            *acc4 = Self::round(*acc4, lane4);
-
+            // SAFETY: We have the right number of bytes and are
+            // handling the unaligned case.
+            let lanes = unsafe { chunk.as_ptr().cast::<[u64; 4]>().read_unaligned() };
+            self.accumulators.write(lanes);
             data = rest;
         }
         let data = data;
@@ -133,25 +166,7 @@ impl XxHash64 {
         let mut acc = if self.length < 32 {
             self.seed.wrapping_add(PRIME64_5)
         } else {
-            let [acc1, acc2, acc3, acc4] = self.accumulators;
-
-            let mut acc = {
-                let acc1 = acc1.rotate_left(1);
-                let acc2 = acc2.rotate_left(7);
-                let acc3 = acc3.rotate_left(12);
-                let acc4 = acc4.rotate_left(18);
-
-                acc1.wrapping_add(acc2)
-                    .wrapping_add(acc3)
-                    .wrapping_add(acc4)
-            };
-
-            acc = Self::merge_accumulator(acc, acc1);
-            acc = Self::merge_accumulator(acc, acc2);
-            acc = Self::merge_accumulator(acc, acc3);
-            acc = Self::merge_accumulator(acc, acc4);
-
-            acc
+            self.accumulators.finish()
         };
 
         // Step 4. Add input length
@@ -164,7 +179,7 @@ impl XxHash64 {
             let lane = u64::from_ne_bytes(*chunk);
             // todo: little-endian
 
-            acc ^= Self::round(0, lane);
+            acc ^= round(0, lane);
             acc = acc.rotate_left(27).wrapping_mul(PRIME64_1);
             acc = acc.wrapping_add(PRIME64_4);
             remaining = rest;
@@ -199,18 +214,12 @@ impl XxHash64 {
 
         acc
     }
+}
 
-    fn round(mut acc: u64, lane: u64) -> u64 {
-        acc = acc.wrapping_add(lane.wrapping_mul(PRIME64_2));
-        acc = acc.rotate_left(31);
-        acc.wrapping_mul(PRIME64_1)
-    }
-
-    fn merge_accumulator(mut acc: u64, acc_n: u64) -> u64 {
-        acc ^= Self::round(0, acc_n);
-        acc = acc.wrapping_mul(PRIME64_1);
-        acc.wrapping_add(PRIME64_4)
-    }
+const fn round(mut acc: u64, lane: u64) -> u64 {
+    acc = acc.wrapping_add(lane.wrapping_mul(PRIME64_2));
+    acc = acc.rotate_left(31);
+    acc.wrapping_mul(PRIME64_1)
 }
 
 trait IntoU64 {
