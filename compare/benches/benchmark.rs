c0fdd56a46145f5c213f4a4017938da9d164d102
Jake Goulding <jake.goulding@gmail.com>
2024-06-27 14:28:29 -0400

    benchmark

--- compare/benches/benchmark.rs
+++ compare/benches/benchmark.rs
@@ -6,7 +6,10 @@ use xx_hash_sys as c;
 use xx_renu as rust;
 
 const TINY_DATA_SIZE: usize = 32;
-const BIG_DATA_SIZE: usize = 100 * 1024 * 1024;
+const BIG_DATA_SIZE: usize = 4 * 1024 * 1024;
+const MIN_BIG_DATA_SIZE: usize = 256 * 1024;
+const MAX_CHUNKS: usize = 64;
+const SEED: u64 = 0xc651_4843_1995_363f;
 
 fn tiny_data(c: &mut Criterion) {
     let (seed, data) = gen_data(TINY_DATA_SIZE);
@@ -16,7 +19,7 @@ fn tiny_data(c: &mut Criterion) {
         let data = &data[..size];
         g.throughput(Throughput::Bytes(data.len() as _));
 
-        let id = format!("c/oneshot/{size}");
+        let id = format!("impl-c/fn-oneshot/size-{size:02}");
         g.bench_function(id, |b| {
             b.iter(|| {
                 let hash = c::XxHash64::oneshot(seed, data);
@@ -24,7 +27,7 @@ fn tiny_data(c: &mut Criterion) {
             })
         });
 
-        let id = format!("c/streaming/{size}");
+        let id = format!("impl-c/fn-streaming/size-{size:02}");
         g.bench_function(id, |b| {
             b.iter(|| {
                 let hash = {
@@ -36,7 +39,7 @@ fn tiny_data(c: &mut Criterion) {
             })
         });
 
-        let id = format!("rust/oneshot/{size}");
+        let id = format!("impl-rust/fn-oneshot/size-{size:02}");
         g.bench_function(id, |b| {
             b.iter(|| {
                 let hash = rust::XxHash64::oneshot(seed, data);
@@ -44,7 +47,7 @@ fn tiny_data(c: &mut Criterion) {
             })
         });
 
-        let id = format!("rust/streaming/{size}");
+        let id = format!("impl-rust/fn-streaming/size-{size:02}");
         g.bench_function(id, |b| {
             b.iter(|| {
                 let hash = {
@@ -64,11 +67,11 @@ fn oneshot(c: &mut Criterion) {
     let (seed, data) = gen_data(BIG_DATA_SIZE);
     let mut g = c.benchmark_group("oneshot");
 
-    for size in half_sizes(&data).take(10) {
+    for size in half_sizes(data.len()).take_while(|&s| s >= MIN_BIG_DATA_SIZE) {
         let data = &data[..size];
         g.throughput(Throughput::Bytes(data.len() as _));
 
-        let id = format!("c/{size}");
+        let id = format!("impl-c/size-{size:07}");
         g.bench_function(id, |b| {
             b.iter(|| {
                 let hash = c::XxHash64::oneshot(seed, data);
@@ -76,7 +79,7 @@ fn oneshot(c: &mut Criterion) {
             })
         });
 
-        let id = format!("rust/{size}");
+        let id = format!("impl-rust/size-{size:07}");
         g.bench_function(id, |b| {
             b.iter(|| {
                 let hash = rust::XxHash64::oneshot(seed, data);
@@ -88,39 +91,42 @@ fn oneshot(c: &mut Criterion) {
     g.finish();
 }
 
-fn streaming_one_chunk(c: &mut Criterion) {
-    let (seed, data) = gen_data(BIG_DATA_SIZE);
-    let mut g = c.benchmark_group("streaming_one_chunk");
+fn streaming(c: &mut Criterion) {
+    let mut g = c.benchmark_group("streaming_many_chunks");
 
-    for size in half_sizes(&data).take(10) {
-        let data = &data[..size];
-        g.throughput(Throughput::Bytes(data.len() as _));
+    for size in half_sizes(BIG_DATA_SIZE).take_while(|&s| s >= MIN_BIG_DATA_SIZE) {
+        for n_chunks in half_sizes(MAX_CHUNKS) {
+            let (seed, chunks) = gen_chunked_data(size, n_chunks);
+            g.throughput(Throughput::Bytes(size as _));
 
-        let id = format!("c/{size}");
-        g.bench_function(id, |b| {
-            b.iter(|| {
-                let mut hasher = c::XxHash64::with_seed(seed);
-                hasher.write(data);
-                let hash = hasher.finish();
-                black_box(hash);
-            })
-        });
+            let id = format!("impl-c/size-{size:07}/chunks-{n_chunks:02}");
+            g.bench_function(id, |b| {
+                b.iter(|| {
+                    let mut hasher = c::XxHash64::with_seed(seed);
+                    for chunk in &chunks {
+                        hasher.write(chunk);
+                    }
+                    let hash = hasher.finish();
+                    black_box(hash);
+                })
+            });
 
-        let id = format!("rust/{size}");
-        g.bench_function(id, |b| {
-            b.iter(|| {
-                let mut hasher = rust::XxHash64::with_seed(seed);
-                hasher.write(data);
-                let hash = hasher.finish();
-                black_box(hash);
-            })
-        });
+            let id = format!("impl-rust/size-{size:07}/chunks-{n_chunks:02}");
+            g.bench_function(id, |b| {
+                b.iter(|| {
+                    let mut hasher = rust::XxHash64::with_seed(seed);
+                    for chunk in &chunks {
+                        hasher.write(chunk);
+                    }
+                    let hash = hasher.finish();
+                    black_box(hash);
+                })
+            });
+        }
     }
 
     g.finish();
 }
-
-const SEED: u64 = 0xc651_4843_1995_363f;
 
 fn gen_data(length: usize) -> (u64, Vec<u8>) {
     let mut rng = rand::rngs::StdRng::seed_from_u64(SEED);
@@ -133,18 +139,32 @@ fn gen_data(length: usize) -> (u64, Vec<u8>) {
     (seed, data)
 }
 
-fn half_sizes(data: &[u8]) -> impl Iterator<Item = usize> {
-    iter::successors(
-        Some(data.len()),
-        |&v| {
-            if v == 1 {
-                None
-            } else {
-                Some(v / 2)
-            }
-        },
-    )
+fn gen_chunked_data(length: usize, n_chunks: usize) -> (u64, Vec<Vec<u8>>) {
+    assert!(length > n_chunks);
+
+    let mut rng = rand::rngs::StdRng::seed_from_u64(SEED);
+
+    let seed = rng.gen();
+
+    let chunk_size = length / n_chunks;
+
+    let mut total = 0;
+    let mut chunks = Vec::with_capacity(2 * n_chunks);
+
+    while total < length {
+        let mut data = vec![0; chunk_size];
+        rng.fill_bytes(&mut data);
+
+        total += data.len();
+        chunks.push(data)
+    }
+
+    (seed, chunks)
 }
 
-criterion_group!(benches, tiny_data, oneshot, streaming_one_chunk);
+fn half_sizes(max: usize) -> impl Iterator<Item = usize> {
+    iter::successors(Some(max), |&v| if v == 1 { None } else { Some(v / 2) })
+}
+
+criterion_group!(benches, tiny_data, oneshot, streaming);
 criterion_main!(benches);
