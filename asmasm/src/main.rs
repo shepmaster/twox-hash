010cc9667d1d50a08a823f25d6923699f026f3d5
Jake Goulding <jake.goulding@gmail.com>
2024-07-10 14:30:14 -0400

    asm compare for C

--- asmasm/src/main.rs
+++ asmasm/src/main.rs
@@ -1,12 +1,25 @@
 use std::{hint::black_box, time::Instant};
+use xx_hash_sys::XxHash3_64 as C;
 use xx_renu::xxhash3_64::XxHash3_64;
 
 fn main() {
     let filename = std::env::args().nth(1).expect("filename");
+    let use_c = std::env::args()
+        .nth(2)
+        .map_or(false, |a| a.eq_ignore_ascii_case("C"));
     let file = std::fs::read(filename).expect("read");
-    let start = Instant::now();
-    let hash = XxHash3_64::oneshot(&file);
-    let elapsed = start.elapsed();
-    black_box(hash);
-    eprintln!("{elapsed:?}");
+
+    if use_c {
+        let start = Instant::now();
+        let hash = C::oneshot(&file);
+        let elapsed = start.elapsed();
+        black_box(hash);
+        eprintln!("C    {elapsed:?}");
+    } else {
+        let start = Instant::now();
+        let hash = XxHash3_64::oneshot(&file);
+        let elapsed = start.elapsed();
+        black_box(hash);
+        eprintln!("Rust {elapsed:?}");
+    }
 }
