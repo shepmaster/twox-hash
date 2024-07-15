4ca95734d7d1d22953d73dc4acff74b99fea1880
Jake Goulding <jake.goulding@gmail.com>
2024-07-15 09:52:40 -0400

    organize

--- src/xxhash3_64.rs
+++ src/xxhash3_64.rs
@@ -319,43 +319,99 @@ fn round_accumulate(acc: &mut [u64; 8], block: &[u8], secret: &[u8]) {
 }
 
 #[inline]
+#[cfg(not(target_arch = "aarch64"))]
 fn round_scramble(acc: &mut [u64; 8], secret: &[u8]) {
+    let last = secret
+        .last_chunk::<{ mem::size_of::<[u8; 64]>() }>()
+        .unwrap();
+    let (last, _) = last.bp_as_chunks();
+    let last = last.iter().copied().map(u64::from_ne_bytes);
 
-
-    // let last = secret
-    //     .last_chunk::<{ mem::size_of::<[u8; 64]>() }>()
-    //     .unwrap();
-    // let (last, _) = last.bp_as_chunks();
-    // let last = last.iter().copied().map(u64::from_ne_bytes);
-
-    // for (acc, secret) in acc.iter_mut().zip(last) {
-    //     *acc ^= *acc >> 47;
-    //     *acc ^= secret;
-    //     *acc = acc.wrapping_mul(PRIME32_1);
-    // }
-
-    unsafe {
-        use core::arch::aarch64::*;
-
-        let secret_base = secret.as_ptr().add(secret.len()).sub(64).cast::<u64>();
-        let (acc, _) = acc.bp_as_chunks_mut::<2>();
-        for (i, acc) in acc.iter_mut().enumerate() {
-            let mut accv = vld1q_u64(acc.as_ptr());
-            let secret = vld1q_u64(secret_base.add(i * 2));
-
-            let shifted = vshrq_n_u64::<47>(accv);
-            accv = veorq_u64(accv, shifted);
-            accv = veorq_u64(accv, secret);
-
-            accv = neon::xx_vmulq_u32_u64(accv, PRIME32_1 as u32);
-
-            vst1q_u64(acc.as_mut_ptr(), accv);
-        }
+    for (acc, secret) in acc.iter_mut().zip(last) {
+        *acc ^= *acc >> 47;
+        *acc ^= secret;
+        *acc = acc.wrapping_mul(PRIME32_1);
     }
 }
 
+#[cfg(target_arch = "aarch64")]
+use neon::{accumulate, round_scramble};
+
 mod neon {
     use core::arch::aarch64::*;
+
+    use super::{SliceBackport as _, PRIME32_1};
+
+    #[inline]
+    pub fn round_scramble(acc: &mut [u64; 8], secret: &[u8]) {
+        unsafe {
+            let secret_base = secret.as_ptr().add(secret.len()).sub(64).cast::<u64>();
+            let (acc, _) = acc.bp_as_chunks_mut::<2>();
+            for (i, acc) in acc.iter_mut().enumerate() {
+                let mut accv = vld1q_u64(acc.as_ptr());
+                let secret = vld1q_u64(secret_base.add(i * 2));
+
+                // tmp[i] = acc[i] >> 47
+                let shifted = vshrq_n_u64::<47>(accv);
+
+                // acc[i] ^= tmp[i]
+                accv = veorq_u64(accv, shifted);
+
+                // acc[i] ^= secret[i]
+                accv = veorq_u64(accv, secret);
+
+                // acc[i] *= PRIME32_1
+                accv = xx_vmulq_u32_u64(accv, PRIME32_1 as u32);
+
+                vst1q_u64(acc.as_mut_ptr(), accv);
+            }
+        }
+    }
+
+    // We process 4x u64 at a time as that allows us to completely
+    // fill a `uint64x2_t` with useful values when performing the
+    // `vmull_{high_}u32`.
+    #[inline]
+    pub fn accumulate(acc: &mut [u64; 8], stripe: &[u8; 64], secret: &[u8; 64]) {
+        let (acc2, _) = acc.bp_as_chunks_mut::<4>();
+        for (i, acc) in acc2.into_iter().enumerate() {
+            unsafe {
+                let mut accv_0 = vld1q_u64(acc.as_ptr().cast::<u64>());
+                let mut accv_1 = vld1q_u64(acc.as_ptr().cast::<u64>().add(2));
+                let stripe_0 = vld1q_u64(stripe.as_ptr().cast::<u64>().add(i * 4));
+                let stripe_1 = vld1q_u64(stripe.as_ptr().cast::<u64>().add(i * 4 + 2));
+                let secret_0 = vld1q_u64(secret.as_ptr().cast::<u64>().add(i * 4));
+                let secret_1 = vld1q_u64(secret.as_ptr().cast::<u64>().add(i * 4 + 2));
+
+                // value[i] = stripe[i] ^ secret[i];
+                let value_0 = veorq_u64(stripe_0, secret_0);
+                let value_1 = veorq_u64(stripe_1, secret_1);
+
+                // tmp[i] = value[i] * (value[i] >> 32)
+                let parts_0 = vreinterpretq_u32_u64(value_0);
+                let parts_1 = vreinterpretq_u32_u64(value_1);
+
+                let hi = vuzp1q_u32(parts_0, parts_1);
+                let lo = vuzp2q_u32(parts_0, parts_1);
+
+                let product_0 = vmull_u32(vget_low_u32(hi), vget_low_u32(lo));
+                let product_1 = vmull_high_u32(hi, lo);
+
+                // acc[i] += tmp[i]
+                accv_0 = vaddq_u64(accv_0, product_0);
+                accv_1 = vaddq_u64(accv_1, product_1);
+
+                // acc[i ^ 1] = acc[i ^ 1] + stripe[i];
+                let stripe_rot_0 = vextq_u64::<1>(stripe_0, stripe_0);
+                let stripe_rot_1 = vextq_u64::<1>(stripe_1, stripe_1);
+                accv_0 = vaddq_u64(accv_0, stripe_rot_0);
+                accv_1 = vaddq_u64(accv_1, stripe_rot_1);
+
+                vst1q_u64(acc.as_mut_ptr().cast::<u64>(), accv_0);
+                vst1q_u64(acc.as_mut_ptr().cast::<u64>().add(2), accv_1);
+            };
+        }
+    }
 
     // There is no `vmulq_u64` (multiply 64-bit by 64-bit, keeping the
     // lower 64 bits of the result) operation, so we have to make our
@@ -416,6 +472,11 @@ mod neon {
             vmlal_u32(high_shifted, input_lo, factor)
         }
     }
+
+    // unsafe {
+    //   _prefetch::<_PREFETCH_READ, _PREFETCH_LOCALITY3>(stripe.as_ptr().cast());
+    //   _prefetch::<_PREFETCH_READ, _PREFETCH_LOCALITY3>(secret.as_ptr().cast());
+    // }
 }
 
 #[inline]
@@ -461,83 +522,17 @@ fn final_merge(acc: &mut [u64; 8], init_value: u64, secret: &[u8], secret_offset
 }
 
 #[inline]
+#[cfg(not(target_arch = "aarch64"))]
 fn accumulate(acc: &mut [u64; 8], stripe: &[u8; 64], secret: &[u8; 64]) {
-    use core::arch::aarch64::*;
+    for i in 0..8 {
+        // TODO: Should these casts / reads happen outside this function?
+        let stripe = unsafe { stripe.as_ptr().cast::<u64>().add(i).read_unaligned() };
+        let secret = unsafe { secret.as_ptr().cast::<u64>().add(i).read_unaligned() };
 
-    // unsafe {
-    //     _prefetch::<_PREFETCH_READ, _PREFETCH_LOCALITY3>(stripe.as_ptr().cast());
-    //     _prefetch::<_PREFETCH_READ, _PREFETCH_LOCALITY3>(secret.as_ptr().cast());
-    // }
-
-    // eprintln!("{acc:x?}");
-    // for i in 0..8 {
-    //     // TODO: Should these casts / reads happen outside this function?
-    //     let stripe = unsafe { stripe.as_ptr().cast::<u64>().add(i).read_unaligned() };
-    //     let secret = unsafe { secret.as_ptr().cast::<u64>().add(i).read_unaligned() };
-
-    //     eprintln!("{:x?}, {:x?}", stripe, secret);
-
-    //     let value = stripe ^ secret;
-    //     acc[i ^ 1] = acc[i ^ 1].wrapping_add(stripe);
-    //     acc[i] = multiply_64_as_32_and_add(value, value >> 32, acc[i]);
-    // }
-
-    // We process 4x u64 at a time as that allows us to completely
-    // fill a `uint64x2_t` with useful values when performing the
-    // `vmull_{high_}u32`.
-    let (acc2, _) = acc.bp_as_chunks_mut::<4>();
-    for (i, acc) in acc2.into_iter().enumerate() {
-        unsafe {
-            let mut accv_0 = vld1q_u64(acc.as_ptr().cast::<u64>());
-            let mut accv_1 = vld1q_u64(acc.as_ptr().cast::<u64>().add(2));
-            let stripe_0 = vld1q_u64(stripe.as_ptr().cast::<u64>().add(i * 4));
-            let stripe_1 = vld1q_u64(stripe.as_ptr().cast::<u64>().add(i * 4 + 2));
-            let secret_0 = vld1q_u64(secret.as_ptr().cast::<u64>().add(i * 4));
-            let secret_1 = vld1q_u64(secret.as_ptr().cast::<u64>().add(i * 4 + 2));
-
-            let value_0 = veorq_u64(stripe_0, secret_0);
-            let value_1 = veorq_u64(stripe_1, secret_1);
-
-            let parts_0 = vreinterpretq_u32_u64(value_0);
-            let parts_1 = vreinterpretq_u32_u64(value_1);
-
-            let hi = vuzp1q_u32(parts_0, parts_1);
-            let lo = vuzp2q_u32(parts_0, parts_1);
-
-            let product_0 = vmull_u32(vget_low_u32(hi), vget_low_u32(lo));
-            let product_1 = vmull_high_u32(hi, lo);
-
-            accv_0 = vaddq_u64(accv_0, product_0);
-            accv_1 = vaddq_u64(accv_1, product_1);
-
-            let stripe_rot_0 = vextq_u64::<1>(stripe_0, stripe_0);
-            let stripe_rot_1 = vextq_u64::<1>(stripe_1, stripe_1);
-            accv_0 = vaddq_u64(accv_0, stripe_rot_0);
-            accv_1 = vaddq_u64(accv_1, stripe_rot_1);
-
-            vst1q_u64(acc.as_mut_ptr().cast::<u64>(), accv_0);
-            vst1q_u64(acc.as_mut_ptr().cast::<u64>().add(2), accv_1);
-        };
+        let value = stripe ^ secret;
+        acc[i ^ 1] = acc[i ^ 1].wrapping_add(stripe);
+        acc[i] = multiply_64_as_32_and_add(value, value >> 32, acc[i]);
     }
-
-
-    // Pseudo-SIMD
-
-    // for (acc, (str, sec)) in acc.iter_mut().zip(stripe_x.into_iter().zip(secret_x)) {
-    //     let value = str ^ sec;
-    //     *acc = multiply_64_as_32_and_add(value, value >> 32, *acc);
-    // }
-
-    // let mut stripe_x = stripe_x;
-
-    // stripe_x.swap(0, 1);
-    // stripe_x.swap(2, 3);
-    // stripe_x.swap(4, 5);
-    // stripe_x.swap(6, 7);
-
-    // for (acc, str) in acc.iter_mut().zip(stripe_x) {
-    //     *acc = acc.wrapping_add(str);
-    // }
 }
 
 #[inline]
