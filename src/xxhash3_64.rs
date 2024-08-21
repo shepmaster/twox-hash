31a751c9cdcbc49d45a2282f95c8309ee276f743
Jake Goulding <jake.goulding@gmail.com>
2024-08-21 11:00:18 -0400

    x86

--- src/xxhash3_64.rs
+++ src/xxhash3_64.rs
@@ -1456,35 +1456,35 @@ mod avx2 {
         let secret = secret.as_ptr().cast::<__m256i>();
 
         unsafe {
-        for i in 0..2 {
-            // [align-acc]: The C code aligns the accumulator to avoid
-            // the unaligned load and store here, but that doesn't
-            // seem to be a big performance loss.
-            let mut acc_0 = _mm256_loadu_si256(acc.add(i));
-            let stripe_0 = _mm256_loadu_si256(stripe.add(i));
-            let secret_0 = _mm256_loadu_si256(secret.add(i));
+            for i in 0..2 {
+                // [align-acc]: The C code aligns the accumulator to avoid
+                // the unaligned load and store here, but that doesn't
+                // seem to be a big performance loss.
+                let mut acc_0 = _mm256_loadu_si256(acc.add(i));
+                let stripe_0 = _mm256_loadu_si256(stripe.add(i));
+                let secret_0 = _mm256_loadu_si256(secret.add(i));
 
-            // let value[i] = stripe[i] ^ secret[i];
-            let value_0 = _mm256_xor_si256(stripe_0, secret_0);
+                // let value[i] = stripe[i] ^ secret[i];
+                let value_0 = _mm256_xor_si256(stripe_0, secret_0);
 
-            // stripe_swap[i] = stripe[i ^ 1]
-            let stripe_swap_0 = _mm256_shuffle_epi32::<0b01_00_11_10>(stripe_0);
+                // stripe_swap[i] = stripe[i ^ 1]
+                let stripe_swap_0 = _mm256_shuffle_epi32::<0b01_00_11_10>(stripe_0);
 
-            // acc[i] += stripe_swap[i]
-            acc_0 = _mm256_add_epi64(acc_0, stripe_swap_0);
+                // acc[i] += stripe_swap[i]
+                acc_0 = _mm256_add_epi64(acc_0, stripe_swap_0);
 
-            // value_shift[i] = value[i] >> 32
-            let value_shift_0 = _mm256_srli_epi64::<32>(value_0);
+                // value_shift[i] = value[i] >> 32
+                let value_shift_0 = _mm256_srli_epi64::<32>(value_0);
 
-            // product[i] = lower_32_bit(value[i]) * lower_32_bit(value_shift[i])
-            let product_0 = _mm256_mul_epu32(value_0, value_shift_0);
+                // product[i] = lower_32_bit(value[i]) * lower_32_bit(value_shift[i])
+                let product_0 = _mm256_mul_epu32(value_0, value_shift_0);
 
-            // acc[i] += product[i]
-            acc_0 = _mm256_add_epi64(acc_0, product_0);
+                // acc[i] += product[i]
+                acc_0 = _mm256_add_epi64(acc_0, product_0);
 
-            _mm256_storeu_si256(acc.add(i), acc_0);
+                _mm256_storeu_si256(acc.add(i), acc_0);
+            }
         }
-    }
     }
 }
 
@@ -1535,33 +1535,33 @@ mod sse2 {
         let secret = secret.as_ptr().cast::<__m128i>();
 
         unsafe {
-        for i in 0..4 {
-            // See [align-acc].
-            let mut acc_0 = _mm_loadu_si128(acc.add(i));
-            let stripe_0 = _mm_loadu_si128(stripe.add(i));
-            let secret_0 = _mm_loadu_si128(secret.add(i));
+            for i in 0..4 {
+                // See [align-acc].
+                let mut acc_0 = _mm_loadu_si128(acc.add(i));
+                let stripe_0 = _mm_loadu_si128(stripe.add(i));
+                let secret_0 = _mm_loadu_si128(secret.add(i));
 
-            // let value[i] = stripe[i] ^ secret[i];
-            let value_0 = _mm_xor_si128(stripe_0, secret_0);
+                // let value[i] = stripe[i] ^ secret[i];
+                let value_0 = _mm_xor_si128(stripe_0, secret_0);
 
-            // stripe_swap[i] = stripe[i ^ 1]
-            let stripe_swap_0 = _mm_shuffle_epi32::<0b01_00_11_10>(stripe_0);
+                // stripe_swap[i] = stripe[i ^ 1]
+                let stripe_swap_0 = _mm_shuffle_epi32::<0b01_00_11_10>(stripe_0);
 
-            // acc[i] += stripe_swap[i]
-            acc_0 = _mm_add_epi64(acc_0, stripe_swap_0);
+                // acc[i] += stripe_swap[i]
+                acc_0 = _mm_add_epi64(acc_0, stripe_swap_0);
 
-            // value_shift[i] = value[i] >> 32
-            let value_shift_0 = _mm_srli_epi64::<32>(value_0);
+                // value_shift[i] = value[i] >> 32
+                let value_shift_0 = _mm_srli_epi64::<32>(value_0);
 
-            // product[i] = lower_32_bit(value[i]) * lower_32_bit(value_shift[i])
-            let product_0 = _mm_mul_epu32(value_0, value_shift_0);
+                // product[i] = lower_32_bit(value[i]) * lower_32_bit(value_shift[i])
+                let product_0 = _mm_mul_epu32(value_0, value_shift_0);
 
-            // acc[i] += product[i]
-            acc_0 = _mm_add_epi64(acc_0, product_0);
+                // acc[i] += product[i]
+                acc_0 = _mm_add_epi64(acc_0, product_0);
 
-            _mm_storeu_si128(acc.add(i), acc_0);
+                _mm_storeu_si128(acc.add(i), acc_0);
+            }
         }
-    }
     }
 }
 
